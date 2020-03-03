8fdb45158c14f5f1a49c8b7d0848e375ed96a0ed
James Bozhkov <36495009+jamesb97@users.noreply.github.com>
2020-03-03 13:24:16 -0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:chatapp/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Chat App',
+      home : new HomePage()
+    );  
+  }      
+}
